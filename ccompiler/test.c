f8a80f82ee3633611277b4859a8f42dbbcfa6b91
P. Constantino <pconst167@gmail.com>
2024-06-12 18:11:50 +0100

    cc

--- ccompiler/test.c
+++ ccompiler/test.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-  printf("%d %d %c %s", 1, 2, 'A', "Paulinho");
-
-	//print_unsigned_long(42672124L);
+	print_unsigned_long(42672124L);
 
 
 }
+
+void print_unsigned_long(unsigned long int num) {
+  char *p;
+  p = &num;
+
+
+	printx8(*p);
+	printx8(*(p+1));
+	printx8(*(p+2));
+	printx8(*(p+3));
+
+}
+
+void printx8(char hex) {
+  asm{
+    meta mov d, hex
+    mov bl, [d]
+    call print_u8x
+  }
+}
+void printx16(int hex) {
+  asm{
+    meta mov d, hex
+    mov b, [d]
+    call print_u16x
+  }
+}
+
+void putchar(char c){
+  asm{
+    meta mov d, c
+    mov al, [d]
+    mov ah, al
+    call _putchar
+  }
+}
+
+void include_stdio_asm(){
+  asm{
+    .include "lib/asm/stdio.asm"
+  }
+}
+
