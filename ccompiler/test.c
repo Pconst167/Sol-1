778c9417b21b9daa2bd81c64d99e43ca79dc9491
P. Constantino <pconst167@gmail.com>
2024-06-24 18:22:07 +0100

    cc

--- ccompiler/test.c
+++ ccompiler/test.c
@@ -1,17 +1,7 @@
+#include <stdio.h>
 
 
-struct _FILE{
-  int handle;
-  int filename[256];
-  int mode; // 0: RD, 1: WR, 2: RW, 3: APPEND
-  int loc; // position of seek head
-};
 
-typedef struct _FILE FILE;
-
-
-void fclose(struct FILE *i){
-}
 
 void main(void){
 
