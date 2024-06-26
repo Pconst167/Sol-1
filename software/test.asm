2c962f2e7a7f138dd4a17ded6378da9129ec30ba
Paulo Constantino <pconst167@gmail.com>
2024-06-27 00:50:02 +0100

    micro

--- software/test.asm
+++ software/test.asm
@@ -8,16 +8,92 @@ main:
 
   call printnl
 
-00000000 00000000 10000000 00000010
-  mov a, $8
-  mov g, $2
-  mov cl, 2
-  shr32 ga
-  mov b, g
-  call print_u16x
-  mov b, a
+  mov b, $0
+  mov c, $0
+  mov a, $0
+  mov g, $0
+  sor32 ga, cb
   call print_u16x
   call printnl
+
+
+  mov b, $1
+  mov c, $0
+  mov a, $0
+  mov g, $0
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+
+  mov b, $0
+  mov c, $1
+  mov a, $0
+  mov g, $0
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+  mov b, $0
+  mov c, $0
+  mov a, $1
+  mov g, $0
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+
+
+  mov b, $0
+  mov c, $0
+  mov a, $0
+  mov g, $1
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+
+
+  mov b, $00010000
+  mov c, $0
+  mov a, $0
+  mov g, $0
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+
+  mov b, $0
+  mov c, $00010000
+  mov a, $0
+  mov g, $0
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+
+  mov b, $0
+  mov c, $0
+  mov a, $00010000
+  mov g, $0
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+
+
+  mov b, $0
+  mov c, $0
+  mov a, $0
+  mov g, $00010000
+  sor32 ga, cb
+  call print_u16x
+  call printnl
+
+
+
+
+
 
 
 
