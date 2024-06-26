d5978566cf33f93b233651d62688eebe89134d49
Paulo Constantino <pconst167@gmail.com>
2024-06-27 00:36:51 +0100

    micro

--- software/test.asm
+++ software/test.asm
@@ -8,145 +8,18 @@ main:
 
   call printnl
 
-
-  mov a, $0
-  mov g, $0
-  mov b, $0
-  mov c, $0
-  sub32 cb, ga
-  push b
-  mov b, c
+00000000 00000000 10000000 00000010
+  mov a, $8
+  mov g, $2
+  mov cl, 2
+  shr32 ga
+  mov b, g
   call print_u16x
-  pop b
+  mov b, a
   call print_u16x
   call printnl
 
 
-
-  mov a, $1
-  mov g, $0
-  mov b, $0
-  mov c, $0
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-
-  mov a, $0
-  mov g, $1
-  mov b, $0
-  mov c, $0
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-  mov a, $0
-  mov g, $0
-  mov b, $1
-  mov c, $0
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-
-  mov a, $0
-  mov g, $0
-  mov b, $0
-  mov c, $1
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-
-  mov a, $1
-  mov g, $0
-  mov b, $1
-  mov c, $0
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-
-; ga + cb 
-  mov a, $0
-  mov g, $1
-  mov b, $1
-  mov c, $0
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-; ga + cb 
-  mov a, $1
-  mov g, $0
-  mov b, $FFFF
-  mov c, $FFFF
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-
-; ga + cb 
-  mov a, $1
-  mov g, $0
-  mov b, $FFFF
-  mov c, $1
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
-
-
-; ga + cb 
-  mov a, $123
-  mov g, $45
-  mov b, $123
-  mov c, $F32
-  sub32 cb, ga
-  push b
-  mov b, c
-  call print_u16x
-  pop b
-  call print_u16x
-  call printnl
 
 
 
