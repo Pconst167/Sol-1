f5d21c7706e0d3e5044a9f7f48f17034ef97f2b5
P. Constantino <pconst167@gmail.com>
2024-07-04 21:15:52 +0100

    cc

--- ccompiler/out.asm
+++ ccompiler/out.asm
@@ -7,7 +7,102 @@
 main:
   mov bp, $FFE0 ;
   mov sp, $FFE0 ; Make space for argc(2 bytes) and for 10 pointers in argv (local variables)
+; int i, j; 
+  sub sp, 2
+  sub sp, 2
+; for(i=0;i<80;i++){ 
+_for1_init:
+  lea d, [bp + -1] ; $i
+  push d
+  mov32 cb, $00000000
+  pop d
+  mov [d], b
+_for1_cond:
+  lea d, [bp + -1] ; $i
+  mov b, [d]
+  mov c, 0
+; --- START RELATIONAL
+  push a
+  mov a, b
+  mov32 cb, $00000050
+  cmp a, b
+  slt ; < (signed)
+  pop a
+; --- END RELATIONAL
+  cmp b, 0
+  je _for1_exit
+_for1_block:
+; for(j=0;j<80;j++){ 
+_for2_init:
+  lea d, [bp + -3] ; $j
+  push d
+  mov32 cb, $00000000
+  pop d
+  mov [d], b
+_for2_cond:
+  lea d, [bp + -3] ; $j
+  mov b, [d]
+  mov c, 0
+; --- START RELATIONAL
+  push a
+  mov a, b
+  mov32 cb, $00000050
+  cmp a, b
+  slt ; < (signed)
+  pop a
+; --- END RELATIONAL
+  cmp b, 0
+  je _for2_exit
+_for2_block:
+; putchar('#'); 
+; --- START FUNCTION CALL
+  mov32 cb, $00000023
+  push bl
+  call putchar
+  add sp, 1
+; --- END FUNCTION CALL
+_for2_update:
+  lea d, [bp + -3] ; $j
+  mov b, [d]
+  mov c, 0
+  mov a, b
+  inc b
+  lea d, [bp + -3] ; $j
+  mov [d], b
+  mov b, a
+  jmp _for2_cond
+_for2_exit:
+; putchar('\n'); 
+; --- START FUNCTION CALL
+  mov32 cb, $0000000a
+  push bl
+  call putchar
+  add sp, 1
+; --- END FUNCTION CALL
+_for1_update:
+  lea d, [bp + -1] ; $i
+  mov b, [d]
+  mov c, 0
+  mov a, b
+  inc b
+  lea d, [bp + -1] ; $i
+  mov [d], b
+  mov b, a
+  jmp _for1_cond
+_for1_exit:
   syscall sys_terminate_proc
+
+putchar:
+  enter 0 ; (push bp; mov bp, sp)
+; --- BEGIN INLINE ASM SEGMENT
+  lea d, [bp + 5] ; $c
+  mov al, [d]
+  mov ah, al
+  mov al, 0
+  syscall sys_io      
+; --- END INLINE ASM SEGMENT
+  leave
+  ret
 ; --- END TEXT SEGMENT
 
 ; --- BEGIN DATA SEGMENT
--- ccompiler/test.c
+++ ccompiler/test.c
@@ -2,4 +2,13 @@
 #include <ctype.h>
 
 void main(){
+  int i, j;
+
+  for(i=0;i<30;i++){
+    for(j=0;j<80;j++){
+      putchar('#');
+
+    }
+    putchar('\n');
+  }
 }
