870675eb0f30221e8177946353a3c79ffcb926a3
paco <paulo.constantino@nordicsemi.no>
2024-12-11 07:06:44 +0000

    fpu

--- hardware/FPU/fpu_tb.sv
+++ hardware/FPU/fpu_tb.sv
@@ -50,10 +50,10 @@ module fpu_tb;
     #500ns;
     arst = 0;
 
-    write_operand_a(32'h3a83126f); //  100000000
+    write_operand_a(32'h3a83126f); //  0.001
     write_operand_b(32'h3ee839f1); //  0.45
 
-    ta_set_operation(pa_fpu::op_sqrt);
+    ta_set_operation(pa_fpu::op_log2);
     ta_start_operation();
     ta_read_result(result);
 
