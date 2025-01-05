24caa6b039173da3710383eff08690cf5f7b0df4
paco <paulo.constantino@nordicsemi.no>
2025-01-05 11:11:30 +0000

    dft

--- misc/dft/dft.c
+++ misc/dft/dft.c
@@ -3,20 +3,22 @@
 
 //  X[k] = sum{n=0 to N-1}(x_real[n]*exp(-2pi*i*k*n/N))
 //  x_real[n] = (1/N)*sum{k=0 to N-1}(X[k]*exp(2pi*i*k*n/N))
-#define N 16
+#define N 32
 
 void dft(float x_real[N], float X_real[N], float X_img[N]);
 void idft(float x_real[N], float x_img[N], float X_real[N], float X_img[N]);
 
 //float x_real[N] = {4, -3.5549, 2.4142, -1.0583, 0, 0.4725, -0.4142, 0.1407, 0, 0.1407, -0.4142, 0.4725, 0, -1.0583, 2.4142, -3.5549};
 //float x_img[N] = {0.0000, 0.7071, -1.0000, 0.7071, 0.0000, -0.7071, 1.0000, -0.7071, 0.0000, 0.7071, -1.0000, 0.7071, 0.0000, -0.7071, 1.0000, -0.7071};
-float x_real[N] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+float x_real[N] = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0};
 float x_img[N] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 float X_real[N];
 float X_img[N];
 
+FILE *fp;
 
 int main(){
+  fp = fopen("plotdata.dat", "w+");
 
   dft(x_real, X_real, X_img);
 
@@ -31,6 +33,13 @@ int main(){
   for(int n = 0; n < N; n++){
     printf("x_real[%2d] = %8.4f + %8.4fi, M = %6.4f, A = %6.4f\n", n, x_real[n], x_img[n], sqrt(x_real[n]*x_real[n] + x_img[n]*x_img[n]), atan2(x_img[n], x_real[n]));
   }
+
+  fprintf(fp, "# k  X[k]\n");
+  for(int k = 0; k < N; k++){
+    fprintf(fp, "%d  %.5f\n", k, sqrt(X_real[k]*X_real[k] + X_img[k]*X_img[k]));
+  }
+
+  fclose(fp);
 }
 
 void dft(float x_real[N], float X_real[N], float X_img[N]){
